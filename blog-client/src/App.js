75265d99df40680fe26330a99d0439def66f1a9d
gregoryann <32854050+gregoryann@users.noreply.github.com>
2023-02-26 13:35:36 +0000

    Add landing svg img

--- blog-client/src/App.js
+++ blog-client/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import PageContent from './components/layout/PageContent';
 import LandingPage from './components/LandingPage';
+import About from './components/About';
 
 function App() {
     return (
         <>
             <PageContent>
                 <LandingPage />
+                <About />
             </PageContent>
         </>
     );
 }
-
 export default App;
