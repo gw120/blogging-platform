6c6409f6136f88af60374456afbea3a049d7b33b
gregoryann <32854050+gregoryann@users.noreply.github.com>
2023-02-28 11:02:40 +0000

    Add mongoose & connect to db & create user schema

--- server/src/app.js
+++ server/src/app.js
@@ -4,27 +4,26 @@ const helmet = require('helmet');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+const connectDb = require('./config/db');
 const middlewares = require('./middlewares');
 
 const app = express();
 
+connectDb();
+
 app.use(morgan('common'));
 app.use(helmet());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.CORS_ORIGIN,
 }));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 app.get('/', (req, res) => {
     res.json({ message: 'Hello world!' });
 });
 app.use('/api', require('./api'));
-
 // 404 handler
-
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
-
 module.exports = app;
