5fb2c7452faaa3ce0e22d9aab7f48ad8bc6aa0f0
gregoryann <32854050+gregoryann@users.noreply.github.com>
2023-03-01 14:16:11 +0000

    Add footer component

---
+++ frontend/src/components/pages/Homepage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import LandingPage from '../layout/LandingPage';
+import About from '../layout/About';
+import { Footer } from '../layout/Footer';
+
+function Homepage() {
+    return (
+        <>
+            <LandingPage />
+            <About />
+            <Footer />
+        </>
+    );
+}
+export default Homepage;
