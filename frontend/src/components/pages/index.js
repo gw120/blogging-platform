54b16113c14f65a8f788f30e30e0d13f81be09ac
gregoryann <32854050+gregoryann@users.noreply.github.com>
2023-03-02 11:38:03 +0000

    Add example explore page

---
+++ frontend/src/components/pages/index.js
@@ -0,0 +1,6 @@
+export { default as Homepage } from './Homepage';
+export { default as Login } from './Login';
+export { default as Register } from './Register';
+export { default as ForgotPassword } from './ForgotPassword';
+export { default as NotFound } from './NotFound';
+export { default as Explore } from './Explore';
